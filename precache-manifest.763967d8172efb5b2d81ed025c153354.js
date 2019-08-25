8fed86717f57dda48f326002e3eeb6107af1309f
Abhishek Kumar <urabhisheks@gmail.com>
2019-08-25 19:47:52 +0530

    Updates

---
+++ precache-manifest.763967d8172efb5b2d81ed025c153354.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "131c49f7312723fbf26c87633cc4b63e",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "aae1032d4b180d66052b",
+    "url": "/monsters/static/css/main.5197a4c1.chunk.css"
+  },
+  {
+    "revision": "18d8118ef9d6e7107a33",
+    "url": "/monsters/static/js/2.6a06a03a.chunk.js"
+  },
+  {
+    "revision": "aae1032d4b180d66052b",
+    "url": "/monsters/static/js/main.c1cf8f6a.chunk.js"
+  },
+  {
+    "revision": "b5894781cb1892e6c069",
+    "url": "/monsters/static/js/runtime~main.8560fe3a.js"
+  }
+]);
